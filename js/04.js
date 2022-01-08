b7ea82fad2e1fde051fecb6c4208e3d406cd2619
Lainer Caceres Salas <81586887+Lainercaceres11@users.noreply.github.com>
2022-01-07 21:48:23 -0500

    Add files via upload
    Archivos

---
+++ js/04.js
@@ -0,0 +1,17 @@
+//Instanciamos Vue js
+const app = new Vue({
+    el: '#app',
+    data:{
+        fondo: 'bg-warning',
+        color: false,
+       
+
+    },
+    methods:{
+
+    }
+      
+     
+
+
+})
